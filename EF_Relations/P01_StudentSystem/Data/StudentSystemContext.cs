d13290cba7c429e2b1d848fa0b116fd9c321e7f2
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-04-28 20:30:29 +0300

    Add files via upload

---
+++ EF_Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        { }
+
+        public StudentSystemContext(DbContextOptions option) : base(option)
+        { }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=StudentSystem;user=sa;Password=Password123@jkl#");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.CourseId, x.StudentId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF_Relations/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var db = new StudentSystemContext();
+
+            db.Database.EnsureDeleted();
+
+            db.Database.EnsureCreated();
+        }
+    }
+}
