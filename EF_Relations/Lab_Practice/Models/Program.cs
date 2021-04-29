661e66c5fc982d2c46bb0771174f523dc75ca666
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-04-29 23:43:35 +0300

    Add files via upload

---
+++ EF_Relations/Lab_Practice/Models/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lab_Practice.Models;
+
+namespace Lab_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            var query = db.Employees.Join
+                (
+                db.Employees,
+                x => x.AddressId,
+                x => x.AddressId,
+                (employees, addresses) => new
+                {
+                    FirstName = employees.FirstName,
+                    Text = addresses.Address.AddressText,
+
+                }
+                ) ;
+
+
+
+            foreach (var kvp in query)
+            {
+                Console.Write(kvp.FirstName );
+                Console.Write( kvp.Text);
+                Console.WriteLine();
+
+            }
+            
+
+        }
+    }
+}
