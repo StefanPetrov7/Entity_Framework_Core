6b94b05f5473ba048b19b4a817b173d0a99a3dbc
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-05-07 17:27:12 +0300

    Add files via upload

---
+++ Mapper/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContextDesignTimeFactory()
+        {
+        }
+
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=.;Database=FastFood;user=sa;Password=Password123@jkl#;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
