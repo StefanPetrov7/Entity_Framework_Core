3af105d11a4a556659d026dafc9d79d006e531cf
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-05-22 19:05:23 +0300

    Add files via upload

---
+++ JSON/08. JSON-Processing-Car-Dealer-Skeleton/CarDealer/DTO/ImportSalesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CarDealer.DTO
+{
+    public class ImportSalesModel
+    {
+        public decimal Discount { get; set; }
+
+        public int CarId { get; set; }
+
+        public int CustomerId { get; set; }
+
+    }
+}
